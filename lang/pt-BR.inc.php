c7477a76d55c8fde10a2249f55a30c954068f5d0
Clément OUDOT <clem.oudot@gmail.com>
2010-07-27 15:47:41 +0000

    Add brazilian translation (references #266)

---
+++ lang/pt-BR.inc.php
@@ -0,0 +1,51 @@
+#==============================================================================
+# LTB Self Service Password
+#
+# Copyright (C) 2009 Clement OUDOT
+# Copyright (C) 2009 LTB-project.org
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# GPL License: http://www.gnu.org/licenses/gpl.txt
+#
+#==============================================================================
+
+#==============================================================================
+# Brazilian
+#==============================================================================
+$messages['nophpldap'] = "Você deve instalar o PHP-Ldap para utilizar esta ferramenta";
+$messages['nophpmhash'] = "Você deve instalar o PHP mhash para utilizar o Samba mode";
+$messages['ldaperror'] = "Não foi possível acessar o diretório LDAP";
+$messages['loginrequired'] = "O seu login é necessário";
+$messages['oldpasswordrequired'] = "A sua senha antiga é necessária";
+$messages['newpasswordrequired'] = "A sua nova senha é necessária";
+$messages['confirmpasswordrequired'] = "Confirme a sua nova senha";
+$messages['passwordchanged'] = "A sua senha foi alterada";
+$messages['nomatch'] = "As senhas não coicidem";
+$messages['badcredentials'] = "Login ou senha incorretos";
+$messages['passworderror'] = "A senha foi recusada pelo Diretório LDAP";
+$messages['title'] = "Serviço de senha";
+$messages['login'] = "Login";
+$messages['oldpassword'] = "Senha antiga";
+$messages['newpassword'] = "Senha nova";
+$messages['confirmpassword'] = "Confirma";
+$messages['submit'] = "OK";
+$messages['tooshort'] = "A sua senha é muito curta";
+$messages['toobig'] = "A sua senha é muito grande";
+$messages['minlower'] = "A sua senha não contém letras minúsculas suficientes";
+$messages['minupper'] = "A sua senha não contém letras maiúsculas suficientes";
+$messages['mindigit'] = "A sua senha não contém caracteres suficientes";
+$messages['policy'] = "A sua senha deve respeitar as regras de restrição:";
+$messages['policyminlength'] = "Tamanho mínimo:";
+$messages['policymaxlength'] = "Tamanho máximo:";
+$messages['policyminlower'] = "Mínimo de letras min&uacutes;sculas:";
+$messages['policyminupper'] = "Míaximo de letras maisúsculas:";
+$messages['policymindigit'] = "Caracteres mínimos:";
