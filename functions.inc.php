56b05aeedf1937a21a0830c9761daa39f6e77412
Jonathan CLARKE <jonathan.clarke@normation.com>
2010-07-28 13:15:36 +0000

    Consider password same as old error as critical too

--- functions.inc.php
+++ functions.inc.php
@@ -86,7 +86,7 @@ function stripslashes_if_gpc_magic_quotes( $string ) {
 # Get message criticity
 function get_criticity( $msg ) {
 	
-	if ( preg_match( "/nophpldap|nophpmhash|ldaperror|nomatch|badcredentials|passworderror|tooshort|toobig|minlower|minupper|mindigit|minspecial|forbiddenchars|answermoderror|answernomatch|mailnomatch|tokennotsent|tokennotvalid/" , $msg ) ) {
+	if ( preg_match( "/nophpldap|nophpmhash|ldaperror|nomatch|badcredentials|passworderror|tooshort|toobig|minlower|minupper|mindigit|minspecial|forbiddenchars|sameasold|answermoderror|answernomatch|mailnomatch|tokennotsent|tokennotvalid/" , $msg ) ) {
 		return "critical";
 	}
 	
