aef5ba3256b1c373b6aa5c3d7536d7fc317e1c48
Clément OUDOT <clem.oudot@gmail.com>
2016-10-18 11:33:53 +0200

    Manage different changelog formats

--- github-issues-to-changelog.pl
+++ github-issues-to-changelog.pl
@@ -10,8 +10,32 @@ while(<>) { $input .= "$_\n"; }
 
 my $json = decode_json $input;
 
+# Debian changelog
+print "# Debian\n";
 foreach my $issue (@$json) {
     print "    * gh#" . $issue->{number} .": " . $issue->{title} . "\n";
 }
+print "\n";
+
+# RPM changelog
+print "# RPM\n";
+foreach my $issue (@$json) {
+    print "- gh#" . $issue->{number} .": " . $issue->{title} . "\n";
+}
+print "\n";
+
+# GitHub changelog
+print "# GitHub\n";
+foreach my $issue (@$json) {
+    print "* #" . $issue->{number} .": " . $issue->{title} . "\n";
+}
+print "\n";
+
+# Release contributors
+print "# Contributors\n";
+foreach my $issue (@$json) {
+    print "Author of issue #" . $issue->{number} .": " . $issue->{user}->{login} . "\n";
+}
+
 
 exit 0;
