12bdff4bc377d09832b98270ded2e2f9caf71366
Philippe Lewin <philippe.lewin@gmail.com>
2017-11-07 12:15:11 +0100

    Test change_password function with basic settings

--- tests/LdapClientTest.php
+++ tests/LdapClientTest.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once __DIR__ . '/../lib/vendor/defuse-crypto.phar';
+require_once __DIR__ . '/../lib/functions.inc.php';
+
 class LdapClientTest extends \PHPUnit_Framework_TestCase
 {
     public function testHello()
@@ -10,6 +13,7 @@ class LdapClientTest extends \PHPUnit_Framework_TestCase
         $ldapconn = ldap_connect('localhost');
         if(!$ldapconn) {
             $this->fail("could not connect to ldap server");
+            return null;
         }
         ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
 
@@ -17,8 +21,33 @@ class LdapClientTest extends \PHPUnit_Framework_TestCase
 
         if ($ldapbind) {
             $this->assertTrue(true, 'Bind OK.');
+            return $ldapconn;
         } else {
             $this->fail("could not bind to server");
+            return null;
         }
     }
+
+    /**
+     * @depends testHello
+     */
+    public function testChangePassword($ldapconn)
+    {
+        $dn = 'uid=plewin,ou=People,dc=my-domain,dc=com';
+        $password = 'newpassword';
+        $ad_mode = false;
+        $ad_options = array();
+        $samba_mode = false;
+        $samba_options = array();
+        $shadow_options = array();
+        $hash = 'sha';
+        $hash_options = array();
+        $who_change_password = 'manager';
+        $oldpassword = null;
+
+        $result = change_password( $ldapconn, $dn, $password, $ad_mode, $ad_options, $samba_mode, $samba_options, $shadow_options, $hash, $hash_options, $who_change_password, $oldpassword );
+
+        $this->assertSame($result, 'passwordchanged');
+    }
+
 }
