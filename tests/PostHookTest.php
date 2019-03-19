2e9f4e1ae265d11011ba3be1ca82e60b15c0f97b
Clément OUDOT <clement.oudot@worteks.com>
2019-03-19 09:35:20 +0100

    Create unit test for posthook_command (ltb-project#273)

---
+++ tests/PostHookTest.php
@@ -0,0 +1,21 @@
+<?php
+
+class PostHookTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCommand()
+    {
+        # Load functions
+        require_once("lib/functions.inc.php");
+
+        $login = "jdoe";
+        $newpassword = "jane";
+        $oldpassword = "john";
+        $posthook = "/usr/local/bin/posthook.sh";
+
+        $this->assertEquals("/usr/local/bin/posthook.sh 'jdoe' 'jane' 'john'", posthook_command($posthook, $login, $newpassword, $oldpassword) );
+        $this->assertEquals("/usr/local/bin/posthook.sh 'jdoe' 'jane'", posthook_command($posthook, $login, $newpassword) );
+        $this->assertEquals("/usr/local/bin/posthook.sh 'jdoe' amFuZQ== am9obg==", posthook_command($posthook, $login, $newpassword, $oldpassword, true) );
+        $this->assertEquals("/usr/local/bin/posthook.sh 'jdoe' amFuZQ==", posthook_command($posthook, $login, $newpassword, null, true) );
+    }
+}
+
