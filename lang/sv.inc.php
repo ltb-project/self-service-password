996000854873cd6632ac0f0cb1c9bba1b887be44
donjoh <donald.johansson@ostersund.se>
2019-09-05 19:15:23 +0200

    Translated PHP Upgrade

--- lang/sv.inc.php
+++ lang/sv.inc.php
@@ -22,7 +22,7 @@
 #==============================================================================
 # Swedish
 #==============================================================================
-$messages['phpupgraderequired'] = "PHP upgrade required";
+$messages['phpupgraderequired'] = "PHP måste uppgraderas";
 $messages['nophpldap'] = "Du borde installera PHP LDAP för att använda detta verktyg";
 $messages['nophpmhash'] = "Du borde installera PHP mhash för att använda Samba mode";
 $messages['nokeyphrase'] = "Token encryption requires a random string in keyphrase setting";
