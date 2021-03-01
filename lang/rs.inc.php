185eed70895c7a410f8980fcf82cc6f00b2bc0e6
Aleksandar Spasojevic <aspasojevic@scgm.com>
2021-03-01 15:18:22 +0100

    update Serbian language

--- lang/rs.inc.php
+++ lang/rs.inc.php
@@ -144,4 +144,4 @@ $messages['ldap_cn'] = "nadimak";
 $messages['ldap_givenName'] = "ime";
 $messages['ldap_sn'] = "prezime";
 $messages['ldap_mail'] = "email adresa";
-$messages['questionspopulatehint'] = "Unesi samo korisničko ime da bi ste proverili koja ste pitanja registorovali.";
+$messages['questionspopulatehint'] = "Unesite samo korisničko ime da bi ste proverili koja ste pitanja registorovali.";
