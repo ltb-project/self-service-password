9b2ecf710906e780fa0fdd525f5617b5b8431fbd
Samuel MARTIN MORO <faust64@gmail.com>
2021-05-05 12:49:33 +0200

    fix(sshkeys): don't send mail notification when entry was not changed

--- htdocs/changesshkey.php
+++ htdocs/changesshkey.php
@@ -148,7 +148,7 @@ if ( $result === "" ) {
 #==============================================================================
 # Notify password change
 #==============================================================================
-if ($mail and $notify_on_sshkey_change) {
+if ($result === "sshkeychanged" and $mail and $notify_on_sshkey_change) {
     $data = array( "login" => $login, "mail" => $mail, "sshkey" => $sshkey);
     if ( !send_mail($mailer, $mail, $mail_from, $mail_from_name, $messages["changesshkeysubject"], $messages["changesshkeymessage"].$mail_signature, $data) ) {
         error_log("Error while sending change email to $mail (user $login)");
