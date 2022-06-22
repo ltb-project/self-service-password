169a8c20e1b105ea9f93b868901998af5506350e
Clément OUDOT <clement@oodo.net>
2022-06-22 17:50:30 +0200

    Test entry before getting DN (#648)

--- htdocs/change.php
+++ htdocs/change.php
@@ -116,9 +116,8 @@ if ( $result === "" ) {
 
                 # Get user DN
                 $entry = ldap_first_entry($ldap, $search);
-                $userdn = ldap_get_dn($ldap, $entry);
 
-                if( !$userdn ) {
+                if( !$entry ) {
                     $result = "badcredentials";
                     error_log("LDAP - User $login not found");
                 } else {
@@ -149,6 +148,7 @@ if ( $result === "" ) {
                     }
 
                     $entry = ldap_get_attributes($ldap, $entry);
+                    $userdn = ldap_get_dn($ldap, $entry);
                     $entry['dn'] = $userdn;
 
                     # Bind with old password
