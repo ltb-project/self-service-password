af1cd86fce5d4bcfe477e255b11db0fc1a2db911
Clément OUDOT <clement@oodo.net>
2023-10-13 11:24:12 +0200

    Use LTB LDAP lib in new setattributes pages

--- htdocs/setattributes.php
+++ htdocs/setattributes.php
@@ -21,6 +21,8 @@
 
 # This page is called to set value for an LDAP attribute
 
+require_once("../vendor/autoload.php");
+
 #==============================================================================
 # POST parameters
 #==============================================================================
@@ -59,28 +61,12 @@ if ( ( $result === "" ) and $use_captcha) { $result = global_captcha_check();}
 if ( $result === "" ) {
 
     # Connect to LDAP
-    $ldap = ldap_connect($ldap_url);
-    ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
-    ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
-    if ( $ldap_starttls && !ldap_start_tls($ldap) ) {
-        $result = "ldaperror";
-        error_log("LDAP - Unable to use StartTLS");
-    } else {
+    $ldap_connection = \Ltb\Ldap::connect($ldap_url, $ldap_starttls, $ldap_binddn, $ldap_bindpw, $ldap_network_timeout, $ldap_krb5ccname);
 
-    # Bind
-    if ( isset($ldap_binddn) && isset($ldap_bindpw) ) {
-        $bind = ldap_bind($ldap, $ldap_binddn, $ldap_bindpw);
-    } else {
-        $bind = ldap_bind($ldap);
-    }
+    $ldap = $ldap_connection[0];
+    $result = $ldap_connection[1];
 
-    if ( !$bind ) {
-        $result = "ldaperror";
-        $errno = ldap_errno($ldap);
-        if ( $errno ) {
-            error_log("LDAP - Bind error $errno  (".ldap_error($ldap).")");
-        }
-    } else {
+    if ($ldap) {
 
     # Search for user
     $ldap_filter = str_replace("{login}", $login, $ldap_filter);
@@ -110,12 +96,12 @@ if ( $result === "" ) {
         if ( $errno ) {
             error_log("LDAP - Bind user error $errno (".ldap_error($ldap).")");
         }
-}}}}}}
+}}}}}
 
 #==============================================================================
 # Register attributes
 #==============================================================================
-if ( $result === "" ) {
+if ( !$result ) {
 
     # Rebind as Manager if needed
     if ( $who_change_attributes == "manager" ) {
