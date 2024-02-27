88fa037ed26779e221c070ae9e8bbcd4d3b69f9d
David Coutadeur <david.coutadeur@worteks.com>
2024-02-27 18:59:24 +0100

    add javascript criteria for local ppolicy (#830)

--- htdocs/js/ppolicy.js
+++ htdocs/js/ppolicy.js
@@ -60,6 +60,155 @@
   };
 
 
+  // Generic feature for checkpassword action
+  // check all local policy criteria one by one and display an appropriate button for each
+  $(document).on('checkpassword', function(event, context) {
+    var digit, evType, hasforbidden, i, len, lower, numspechar, password, report, setResult, upper;
+    password = context.password;
+    evType = context.evType;
+    setResult = context.setResult;
+    report = function(result, id) {
+      if (result) {
+        return setResult(id, "good");
+      } else {
+        return setResult(id, "bad");
+      }
+    };
+
+    // Criteria checks
+    if (window.policy.pwd_min_length > 0) {
+      report(password.length >= window.policy.pwd_min_length, 'ppolicy-pwd_min_length-feedback');
+    }
+
+    if (window.policy.pwd_max_length > 0) {
+      report(password.length <= window.policy.pwd_max_length, 'ppolicy-pwd_max_length-feedback');
+    }
+
+    if (window.policy.pwd_min_upper > 0) {
+      upper = password.match(/[A-Z]/g);
+      report(upper && upper.length >= window.policy.pwd_min_upper, 'ppolicy-pwd_min_upper-feedback');
+    }
+
+    if (window.policy.pwd_min_lower > 0) {
+      lower = password.match(/[a-z]/g);
+      report(lower && lower.length >= window.policy.pwd_min_lower, 'ppolicy-pwd_min_lower-feedback');
+    }
+
+    if (window.policy.pwd_min_digit > 0) {
+      digit = password.match(/[0-9]/g);
+      report(digit && digit.length >= window.policy.pwd_min_digit, 'ppolicy-pwd_min_digit-feedback');
+    }
+
+    if (window.policy.pwd_no_reuse && window.policy.pwd_no_reuse == true) {
+      if( $( "#oldpassword" ) && $( "#oldpassword" ).val().length > 0 && password.length > 0 )
+      {
+        oldpassword = $( "#oldpassword" ).val();
+        report( password != oldpassword , 'ppolicy-pwd_no_reuse-feedback');
+      }
+      else
+      {
+        report( true , 'ppolicy-pwd_no_reuse-feedback');
+      }
+    }
+
+    if (window.policy.pwd_diff_login && window.policy.pwd_diff_login == true) {
+      if( $( "#login" ) && $( "#login" ).val().length > 0 && password.length > 0 )
+      {
+        login = $( "#login" ).val();
+        report( password != login, 'ppolicy-pwd_diff_login-feedback');
+      }
+      else
+      {
+        report( true , 'ppolicy-pwd_diff_login-feedback');
+      }
+    }
+
+    if (window.policy.pwd_diff_last_min_chars > 0) {
+      if( $( "#oldpassword" ) && $( "#oldpassword" ).val().length > 0 && password.length > 0 )
+      {
+        minDiffChars = window.policy.pwd_diff_last_min_chars;
+        oldpassword = $( "#oldpassword" ).val();
+        forbidden = false;
+        diffCharCounter = 0;
+        i = 0;
+        while (i < password.length) {
+          if (oldpassword.indexOf(password.charAt(i)) == -1)  {
+            diffCharCounter++;
+          }
+          i++;
+        }
+        report( diffCharCounter >= minDiffChars , 'ppolicy-pwd_diff_last_min_chars-feedback');
+      }
+      else
+      {
+        report( true , 'ppolicy-pwd_diff_last_min_chars-feedback');
+      }
+    }
+
+    if (window.policy.pwd_forbidden_chars) {
+      forbiddenChars = window.policy.pwd_forbidden_chars;
+      forbidden = false;
+      i = 0;
+      while (i < password.length) {
+        if (forbiddenChars.indexOf(password.charAt(i)) != -1)  {
+          forbidden = true;
+        }
+        i++;
+      }
+      report( !forbidden, 'ppolicy-pwd_forbidden_chars-feedback' );
+    }
+
+    if (window.policy.pwd_min_special > 0 && window.policy.pwd_special_chars) {
+      numspechar = 0;
+      var re = new RegExp("["+window.policy.pwd_special_chars+"]","");
+      i = 0;
+      while (i < password.length) {
+        if (password.charAt(i).match(re)) {
+          numspechar++;
+        }
+        i++;
+      }
+      report(numspechar >= window.policy.pwd_min_special, 'ppolicy-pwd_min_special-feedback');
+    }
+
+    if ( window.policy.pwd_no_special_at_ends &&
+         window.policy.pwd_no_special_at_ends == true &&
+         window.policy.pwd_special_chars ) {
+      var re_start = new RegExp("^["+window.policy.pwd_special_chars+"]","");
+      var re_end = new RegExp("["+window.policy.pwd_special_chars+"]$","");
+      report( ( !password.match(re_start) && !password.match(re_end) ) , 'ppolicy-pwd_no_special_at_ends-feedback');
+    }
+
+    if ( window.policy.pwd_complexity) {
+      complexity = 0;
+      if (window.policy.pwd_special_chars) {
+        var re = new RegExp("["+window.policy.pwd_special_chars+"]","");
+        if( password.match(re) ){
+          complexity++;
+        }
+      }
+      if( password.match(/[A-Z]/g) ){
+        complexity++;
+      }
+      if( password.match(/[a-z]/g) ){
+        complexity++;
+      }
+      if( password.match(/[0-9]/g) ){
+        complexity++;
+      }
+      report( complexity >= window.policy.pwd_complexity, 'ppolicy-pwd_complexity-feedback');
+    }
+
+
+    if ( window.policy.use_pwnedpasswords) {
+      setResult('ppolicy-use_pwnedpasswords-feedback', "unknown");
+    }
+
+  });
+
+
+
+  // Specific feature for checkentropy action
   $(document).on('checkpassword', function(event, context) {
     var entropyrequired, entropyrequiredlevel, evType, newpasswordVal, password, setResult;
     password = context.password;
@@ -125,6 +274,8 @@
     }
   });
 
+
+
   checkpassword = function(password, evType) {
     var e, info;
     e = jQuery.Event("checkpassword");
@@ -135,7 +286,7 @@
     };
     return $(document).trigger(e, info);
   };
-  if ( $('#ppolicy-checkentropy-feedback').length && $('#newpassword').length) {
+  if ( (window.policy != null) && $('#newpassword').length) {
     checkpassword('');
     $('#newpassword').keyup(function(e) {
       checkpassword(e.target.value);
