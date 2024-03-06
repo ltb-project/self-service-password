56481eb24a09bc3f86c8e5296863c3068903956e
David Coutadeur <david.coutadeur@worteks.com>
2024-03-06 19:38:43 +0100

    prevent multiple form submits (#757)

--- htdocs/js/self-service-password.js
+++ htdocs/js/self-service-password.js
@@ -5,4 +5,19 @@ $(document).ready(function(){
         placement: 'bottom',
         container: 'body' // Allows the popover to be larger than the menu button
     });
+
+    $("form").on('submit', function (event) {
+
+        var $form = $(this);
+
+        if ($form.data('data-submitted') === true) {
+            // Previously submitted - don't submit again
+            event.preventDefault();
+        } else {
+            // Mark it so that the next submit can be ignored
+            $form.data('data-submitted', true);
+        }
+
+    });
+
 });
