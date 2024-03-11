e5f9c3834966db946d6e7c1523c2ac71222e77e0
David Coutadeur <david.coutadeur@worteks.com>
2024-03-11 10:26:15 +0100

    fix triggering of pwd_no_reuse and pwd_diff_login checks (#854)

--- htdocs/js/ppolicy.js
+++ htdocs/js/ppolicy.js
@@ -100,7 +100,7 @@
     }
 
     if (window.policy.pwd_no_reuse && window.policy.pwd_no_reuse == true) {
-      if( $( "#oldpassword" ) && $( "#oldpassword" ).val().length > 0 && password.length > 0 )
+      if( $( "#oldpassword" ) )
       {
         oldpassword = $( "#oldpassword" ).val();
         report( password != oldpassword , 'ppolicy-pwd_no_reuse-feedback');
@@ -112,7 +112,7 @@
     }
 
     if (window.policy.pwd_diff_login && window.policy.pwd_diff_login == true) {
-      if( $( "#login" ) && $( "#login" ).val().length > 0 && password.length > 0 )
+      if( $( "#login" ) )
       {
         login = $( "#login" ).val();
         report( password != login, 'ppolicy-pwd_diff_login-feedback');
