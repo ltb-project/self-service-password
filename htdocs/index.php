ltb-project/self-service-password

commit de1233ab2506a6cb2f88a0330969bb9e07582df8

Filter allowed languages

htdocs/index.php
@@ -40,7 +40,7 @@ require_once("../lib/detectbrowserlanguage.php");
 $files = glob("../lang/*.php");
 $languages = str_replace(".inc.php", "", $files);
 $languages = str_replace("../lang/", "", $languages);
-$lang = detectLanguage($lang, $languages);
+$lang = detectLanguage($lang, array_intersect($languages,$allowed_lang));
 require_once("../lang/$lang.inc.php");
 
 # Remove default questions

commit 4736100f1f83b6102c863f624a5fb9230186d1c9

Check allowed lang array only if not empty
Fix regression introduced in commit de1233ab2506a6cb2f88a0330969bb9e07582df8

htdocs/index.php
@@ -34,7 +34,7 @@ require_once("../lib/detectbrowserlanguage.php");
 $files = glob("../lang/*.php");
 $languages = str_replace(".inc.php", "", $files);
 $languages = str_replace("../lang/", "", $languages);
-$lang = detectLanguage($lang, array_intersect($languages,$allowed_lang));
+$lang = detectLanguage($lang, $allowed_lang ? array_intersect($languages,$allowed_lang) : $languages);
 require_once("../lang/$lang.inc.php");
 
 # Remove default questions