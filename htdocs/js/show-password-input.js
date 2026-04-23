9212094b07937b24e4bbb0d9d5acda72c79f02f9
Abhishek Pai <abpai94@gmail.com>
2026-04-23 15:33:02 +0200

    #1099: Adding touch events. Adding mouse drag edge case. Minor code refactor.

--- htdocs/js/show-password-input.js
+++ htdocs/js/show-password-input.js
@@ -3,15 +3,17 @@ fields.map((field) => {
     const passwordField = document.getElementById(field);
     const togglePassword = passwordField.parentElement.querySelector(".password-toggle-icon i");
 
-    togglePassword.addEventListener("click", () => {
-        if (passwordField.type === "password") {
+    const showPassword = () => {
             passwordField.type = "text";
-            togglePassword.classList.remove("fa-eye");
-            togglePassword.classList.add("fa-eye-slash");
-        } else {
+            togglePassword.classList.replace("fa-eye", "fa-eye-slash");
+    };
+    const hidePassword = () => {
             passwordField.type = "password";
-            togglePassword.classList.remove("fa-eye-slash");
-            togglePassword.classList.add("fa-eye");
-        }
-    });
+            togglePassword.classList.replace("fa-eye-slash", "fa-eye");
+    };
+    togglePassword.addEventListener("mousedown", showPassword);
+    togglePassword.addEventListener("touchstart", showPassword);
+    togglePassword.addEventListener("mouseup", hidePassword);
+    togglePassword.addEventListener("mouseleave", hidePassword);
+    togglePassword.addEventListener("touchend", hidePassword);
 })
