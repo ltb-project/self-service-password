c10c2ca120461be14271436915afc26c723aeda7
Clément OUDOT <clement.oudot@worteks.com>
2026-09-02 15:11:17 +0200

    New design in doc

--- docs/conf.py
+++ docs/conf.py
@@ -78,7 +78,7 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_logo = 'images/ltb-logo.png'
+html_logo = 'images/ltb_ssp_icon.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
