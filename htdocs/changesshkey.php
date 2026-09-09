c303e484fa6e449732bb634810fa6d92c774fd1d
Clément OUDOT <clement.oudot@worteks.com>
2026-09-09 14:51:58 +0200

    HTML mail for SSH key changed

--- htdocs/changesshkey.php
+++ htdocs/changesshkey.php
@@ -134,7 +134,10 @@ if ( !$result ) {
 if ($result === "sshkeychanged") {
     if ($mail and $notify_on_sshkey_change) {
         $data = array( "login" => $login, "mail" => $mail, "sshkey" => $sshkey);
-        if (! $mailer->send_mail($mail, $mail_from, $mail_from_name, $messages["changesshkeysubject"], $messages["changesshkeymessage"].$mail_signature, $data)) {
+        $smarty->assign("mail_data", $data);
+        $smarty->assign("mail_signature", $mail_signature);
+        $html_body = $smarty->fetch('mails/sshkeychanged.tpl');
+        if ( !$mailer->send_mail($mail, $mail_from, $mail_from_name, $messages["changesubject"], $messages["changemessage"]. $mail_signature, $data, $html_body) ) {
             error_log("Error while sending change email to $mail (user $login)");
         }
     }
